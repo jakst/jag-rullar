8a778842473e23c282b264961e18ef6dc8a99ea6
Jakob Norlin <jakob.stahl91@gmail.com>
2023-01-01 17:42:30 +0100

    Display cars as cards with model info and image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,26 @@ import {
   type GetServerSidePropsContext,
   type InferGetServerSidePropsType,
 } from "next";
+import { Flex, useTheme } from "vcc-ui";
 import carsJsonData from "../public/api/cars.json";
+import { CarDisplayCard } from "../src/components/CarDisplayCard";
 import { HelloWorld } from "../src/components/HelloWorld";
 import { type Car } from "../src/types";
 
-export default function HomePage(props: Props) {
+export default function HomePage({ cars }: Props) {
+  const theme = useTheme();
+
   return (
     <>
       <HelloWorld />
-      <pre>{JSON.stringify(props.cars, null, 2)}</pre>
+
+      <Flex extend={{ flexDirection: "row", gap: theme.baselineGrid * 2 }}>
+        {cars.map((car) => (
+          <CarDisplayCard key={car.id} car={car} />
+        ))}
+      </Flex>
+
+      <pre>{JSON.stringify(cars, null, 2)}</pre>
     </>
   );
 }
---
+++ src/components/CarDisplayCard.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { Text, useTheme, View } from "vcc-ui";
+import { type Car } from "../types";
+
+interface Props {
+  car: Car;
+}
+
+export function CarDisplayCard({ car }: Props) {
+  const theme = useTheme();
+
+  return (
+    <View extend={{ width: 300 }}>
+      <Text
+        variant="columbus"
+        subStyle="emphasis"
+        extend={{
+          textTransform: "uppercase",
+          // The specific style shown in the design draft is not available as a pre-configured
+          // variat/sub-style. Overriding the color seems to be the only option.
+          color: theme.color.foreground.secondary,
+        }}
+      >
+        {car.bodyType}
+      </Text>
+
+      <Text variant="amundsen">
+        {car.modelName}{" "}
+        <Text
+          variant="columbus"
+          // Same as for the body type above
+          extend={{ color: theme.color.foreground.secondary }}
+        >
+          {car.modelType}
+        </Text>
+      </Text>
+
+      {/*
+       w300/h225 matches the w800/h600 dimensions of the image files used here.
+       If these images varied in size we would have to rely on the object-fit CSS-
+       property to avoid image stretching and layout shift simultaneously. */}
+      <Image
+        src={createImageUrl(car.id)}
+        alt={`${car.modelName} viewed from the side, with the front of the car pointing to the left`}
+        width={300}
+        height={225}
+      />
+    </View>
+  );
+}
+
+function createImageUrl(carId: string) {
+  return `/images/${carId.replaceAll("-", "_")}.jpg`;
+}
