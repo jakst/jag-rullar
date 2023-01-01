9c22ce721c553332eb15c576c940298b556a5f0d
Jakob Norlin <jakob.stahl91@gmail.com>
2023-01-01 17:43:14 +0100

    Remove HelloWorld and data output

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import {
 import { Flex, useTheme } from "vcc-ui";
 import carsJsonData from "../public/api/cars.json";
 import { CarDisplayCard } from "../src/components/CarDisplayCard";
-import { HelloWorld } from "../src/components/HelloWorld";
 import { type Car } from "../src/types";
 
 export default function HomePage({ cars }: Props) {
@@ -13,15 +12,11 @@ export default function HomePage({ cars }: Props) {
 
   return (
     <>
-      <HelloWorld />
-
       <Flex extend={{ flexDirection: "row", gap: theme.baselineGrid * 2 }}>
         {cars.map((car) => (
           <CarDisplayCard key={car.id} car={car} />
         ))}
       </Flex>
-
-      <pre>{JSON.stringify(cars, null, 2)}</pre>
     </>
   );
 }
--- src/components/HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "vcc-ui";
-
-export function HelloWorld() {
-  return (
-    <Text variant="yang" as="h1">
-      Hello World!
-    </Text>
-  );
-}
