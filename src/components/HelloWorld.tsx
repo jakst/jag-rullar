a07f1af76e86d6870861eedf3581b9b244399c88
Jakob Norlin <jakob.stahl91@gmail.com>
2023-01-01 16:12:02 +0100

    Set up VCC-UI

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Text } from "vcc-ui";
 
 export const HelloWorld: React.FC = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <Text variant="yang" as="h1">
+      Hello World!
+    </Text>
+  );
 };
